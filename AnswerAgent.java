65059760dabc6364b8f1518a9524d1bd29badfb5
MehmetErenToklu <147389387+MehmetErenToklu@users.noreply.github.com>
2026-03-24 19:48:47 +0300

    Delete AnswerAgent.java

--- AnswerAgent.java
+++
@@ -1,7 +0,0 @@
-package mu.cse.rag.clean;
-
-import java.util.List;
-
-public interface AnswerAgent {
-    String generateAnswer(String question, List<String> terms, List<Hit> hits); //Generates answer based on question and hits
-}
